a367dd7e414af0260bae59c774b0c7cb65930ffb
Axel Vazquez <axelvazmar0@gmail.com>
2026-03-17 22:37:09 -0600

    Objetos literales en TypeScript

---
+++ 01-reforzamiento/src/bases/03-object-literals.ts
@@ -0,0 +1,45 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    address: Address;
+}
+
+interface Address {
+    postalCode: string;
+    city: string;
+}
+
+const ironman: Person = {
+    firstName: 'Tony',
+    lastName: 'Stark',
+    age: 45,
+    address: {
+        postalCode: '1234',
+        city: 'New york'
+    }
+};
+
+const spiderman: Person = {
+    firstName: "Peter",
+    lastName: "Parker",
+    age: 0,
+    address: {
+        postalCode: 'AB123',
+        city: 'New York'
+    }
+}
+
+console.log(ironman);
+
+// const spiderman = { ...ironman }; //
+/* Con los 3 puntos le estamos diciendo que esparza/asigne todos los valores de iron en spiderman, se le conoce como operador speard */
+
+// const spiderman = structuredClone(ironman);
+
+// spiderman.firstName = 'Peter';
+// spiderman.lastName = 'Parker';
+// spiderman.age = 22;
+// spiderman.address.city = 'San José'
+
+// console.log(ironman, spiderman)
