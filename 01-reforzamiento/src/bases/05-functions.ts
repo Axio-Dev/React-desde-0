166883faae2c2152e6236740c852df1f863ff1dc
Axio Dev <axelvazmar0@gmail.com>
2026-03-23 22:26:37 -0600

    Funciones con múltiples retornos

--- 01-reforzamiento/src/bases/05-functions.ts
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -2,28 +2,29 @@ function greet ( name: string ): string { // Aquí estamos obligando a TS a que 
     return `Hola ${name}`
 }
 
-const greet2 = (name: string) => {
-    return `Hola ${name}`
-}
+const greet2 = (name: string) => `Hola ${name}` // Así es como se hace una función de flecha simplificada
 
 greet = function() { // Esto sí se puede en Js cuando es un función normal, pero en TS no se puede
     return 'Hola Pedro';
 }
 
-function getUser() {
+interface User{
+    uid: string;
+    username: string;
+}
+
+function getUser(): User {
     return {
         uid: 'ABC-123',
-        username: 'El_Papi21'
+        username: 'El_Papi21',
     }
 }
 
 // Tarea en vídeo: crear una función de flecha que regrese lo mismo que getUser()
-const getUser2 = () => {
-    return {
-        uid: 'Uid-123',
-        username: 'La_Mami21'
-    }
-}
+const getUser2 = () => ({ // así es como se simplifica una funcion de flecha con un retorno más complejo. Los parentesis fungen como retono implícito
+    uid: 'Uid-123',
+    username: 'La_Mami21'
+});
 
 const message = greet('Goku');
 const message2 = greet2('Vegeta');
@@ -32,3 +33,15 @@ console.log(message, message2);
 const user = getUser();
 const user2 = getUser2();
 console.log(user, user2)
+
+const myArray: number[] = [1,2,3,4,5];
+
+// myArray.forEach(function(value){
+//     console.log({ value })
+// });
+// Manera de hacer una funcion para recorrer e imprimir números, de manera tradicional. Esto se le conoce como una función callback
+
+myArray.forEach( (value) => {
+    console.log({ value })
+});
+// Manera de crear de recorrer un array con una función de flecha (Más simple)
