39702d44bb3d4f1b221e2b5370d4075bca36ca52
Axio Dev <axelvazmar0@gmail.com>
2026-03-29 21:45:23 -0600

    Conociendo el Hook useState, este hook se usa cuando un componente necesita cambiar de estado

--- 02-firsts-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-firsts-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 interface Props {
     name: string;
     quantity?: number;
 }
 
-export const ItemCounter = ({ name, quantity }: Props) => {
+export const ItemCounter = ({ name, quantity = 0 }: Props) => {
 
-    const handleClick = () => {
-        console.log(`Click en ${name}`)
+    const [count, setCount] = useState(quantity);
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+
+    const handleSubtract = () => {
+        if (count <= 0) return;
+        setCount(count - 1)
     }
 
     return (
@@ -19,13 +28,14 @@ export const ItemCounter = ({ name, quantity }: Props) => {
             <span
                 style={{
                     width: 150,
+                    color: count <= 5 ? 'red' : 'green'
                 }}
             >
                 {name}
             </span>
-            <button onClick={handleClick}>+1</button>
-            <span>{quantity}</span>
-            <button> -1 </button>
+            <button onClick={handleAdd}>+1</button>
+            <span>{count}</span>
+            <button onClick={handleSubtract}> -1 </button>
         </section>
     )
 }
