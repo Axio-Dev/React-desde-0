c721d1105ba8dda0dbfeed57444c1c260f257094
Axio Dev <axelvazmar0@gmail.com>
2026-03-31 12:54:25 -0600

    Añadiendo más pruebas unitarias

--- 02-firsts-steps/src/helpers/math.helper.test.ts
+++ 02-firsts-steps/src/helpers/math.helper.test.ts
@@ -1,18 +1,69 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-import { add } from './math.helper';
+import { add, multiply, subtract } from './math.helper';
 
+describe('add', () => {
+    test('Sould add two positives numbers', () => {
+        //! 1. Arrange
+        const a = 1;
+        const b = 2;
 
-test('Sould add two positives numbers', () => {
-    //! 1. Arrange
-    const a = 1;
-    const b = 2;
+        //! 2. Act
+        const result = add(a, b);
 
-    //! 2. Act
-    const result = add(a, b);
+        //! 3. Assert
+        expect(result).toBe(a + b);
 
-    //! 3. Assert
-    expect(result).toBe(a + b);
+    });
+    test('Sould add two negative numbers', () => {
+        //! 1. Arrange
+        const a = -1;
+        const b = -2;
 
+        //! 2. Act
+        const result = add(a, b);
+
+        //! 3. Assert
+        expect(result).toBe(a + b);
+
+    });
 });
 
+
+describe('Subtract', () => {
+    test('Should subtract two positives numbers', () => {
+        const a = 2;
+        const b = 2;
+
+        const result = subtract(a, b);
+
+        expect(result).toBe(a - b)
+    });
+    test('Should subtract two negative numbers', () => {
+        const a = -2;
+        const b = -100;
+
+        const result = subtract(a, b);
+
+        expect(result).toBe(a - b)
+    });
+});
+
+describe("Multiply", () => {
+    test("Should multiply two positives numbers", () => {
+        const a = 3;
+        const b = 10;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b)
+    });
+    test("Should multiply two negative numbers", () => {
+        const a = -3;
+        const b = -10.50;
+
+        const result = multiply(a, b);
+
+        expect(result).toBe(a * b)
+    });
+});
--- 02-firsts-steps/src/helpers/math.helper.ts
+++ 02-firsts-steps/src/helpers/math.helper.ts
@@ -5,11 +5,12 @@ export const add = (a: number, b: number) => {
 }
 
 export const subtract = (a: number, b: number) => {
-    return a + b;
+    return a - b;
 }
 
 export const multiply = (a: number, b: number) => {
     return a * b;
+
 }
 
 export const divide = (a: number, b: number) => {
