a70d52fa858e47d7c5ba65710c899a68c1c165d8
Axio Dev <axelvazmar0@gmail.com>
2026-04-04 15:24:28 -0600

    Testeando argumentos específicos de los componentes

--- 02-firsts-steps/src/FirstSteps.test.tsx
+++ 02-firsts-steps/src/FirstSteps.test.tsx
@@ -1,12 +1,31 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, test, vi } from 'vitest';
+import { describe, expect, test, vi, afterEach } from 'vitest';
 import { FirstStepsApp } from './FirstsStepsApp';
 
+const mockItemCounter = vi.fn((_props: unknown) => {
+  /* Extrayendo el componente ItemCounter dentro de una fn para saber cuantas veces fue llamado */
+  return <div data-testid="ItemCounter" />;
+}); //.fn() es una funcion que cuando se manda a llamar nos dice con que argumentos fue llamada y simular peticiones HTTP
+
 vi.mock('./shopping-cart/ItemCounter.tsx', () => ({
-  ItemCounter: () => <div data-testid="ItemCounter" />,
-})); // Creando la simulación de un componente
+  ItemCounter: (props: unknown) => mockItemCounter(props),
+}));
+
+// vi.mock('./shopping-cart/ItemCounter.tsx', () => ({
+//   ItemCounter: (props: unknown) => (
+// <div
+//   data-testid="ItemCounter"
+//   name={props.name}
+//   quantity={props.quantity}
+// />
+//   ),
+// })); // Creando la simulación de un componente
 
 describe('FirstSteps', () => {
+  afterEach(() => {
+    vi.clearAllMocks(); // Limpiando las pruebas para que no sean dependientes unas de otras
+  });
+
   test('should match snpashot', () => {
     const { container } = render(<FirstStepsApp />);
     expect(container).toMatchSnapshot();
@@ -17,7 +36,24 @@ describe('FirstSteps', () => {
 
     const itemCounters = screen.getAllByTestId('ItemCounter');
     expect(itemCounters.length).toBe(3);
+  });
 
-    screen.debug();
+  test('should render ItemCounter with correct props', () => {
+    render(<FirstStepsApp />);
+
+    expect(mockItemCounter).toHaveBeenCalledTimes(3);
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      // Testeando con que argumentos se llamó al componente
+      name: 'Nintendo Switch 2',
+      quantity: 1,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: 'Pro Controller',
+      quantity: 3,
+    });
+    expect(mockItemCounter).toHaveBeenCalledWith({
+      name: 'Super Smash',
+      quantity: 5,
+    });
   });
 });
