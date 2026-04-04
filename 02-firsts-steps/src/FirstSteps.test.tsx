882c7aa505dc36542dd24e4a5bc64217dd362b22
Axio Dev <axelvazmar0@gmail.com>
2026-04-04 14:54:40 -0600

    Creando pruebas apartir de un componente mock (Replicar un componente real con uno ficticio)

--- 02-firsts-steps/src/FirstSteps.test.tsx
+++ 02-firsts-steps/src/FirstSteps.test.tsx
@@ -1,14 +1,23 @@
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
-import { FirstStepsApp } from "./FirstsStepsApp";
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { FirstStepsApp } from './FirstsStepsApp';
 
-
+vi.mock('./shopping-cart/ItemCounter.tsx', () => ({
+  ItemCounter: () => <div data-testid="ItemCounter" />,
+})); // Creando la simulación de un componente
 
 describe('FirstSteps', () => {
+  test('should match snpashot', () => {
+    const { container } = render(<FirstStepsApp />);
+    expect(container).toMatchSnapshot();
+  });
 
-    test('should match snpashot', () => {
+  test('should render the correct number of ItemCounter components', () => {
+    render(<FirstStepsApp />);
 
-        const { container } = render(<FirstStepsApp />);
-        expect(container).toMatchSnapshot();
-    })
-})
+    const itemCounters = screen.getAllByTestId('ItemCounter');
+    expect(itemCounters.length).toBe(3);
+
+    screen.debug();
+  });
+});
