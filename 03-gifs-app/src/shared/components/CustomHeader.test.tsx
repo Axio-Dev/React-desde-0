8ac4a8b85869acae1b902244d8e1caab32dc0e27
Axio Dev <axelvazmar0@gmail.com>
2026-05-31 01:15:06 -0600

    Adding test cases to CustomHeader

---
+++ 03-gifs-app/src/shared/components/CustomHeader.test.tsx
@@ -0,0 +1,30 @@
+import { describe, test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { CustomHeader } from "./CustomHeader";
+
+describe("CustomHeader", () => {
+  const title = "This is a title";
+
+  test("should render the title correctly", () => {
+    render(<CustomHeader title={title} />);
+    expect(screen.getByText(title)).toBeDefined;
+  });
+
+  test("should render the description when provided", () => {
+    const description = "This a provided description";
+    render(<CustomHeader title={title} description={description} />);
+    expect(screen.getByText(description)).toBeDefined();
+    expect(screen.getByRole("paragraph")).toBeDefined();
+    expect(screen.getByRole("paragraph").innerHTML).toBe(description);
+  });
+
+  test("should not render description when not provided", () => {
+    const { container } = render(<CustomHeader title={title} />);
+    const divElement = container.querySelector(".content-center");
+    const h1 = divElement?.querySelector("h1");
+    expect(h1?.innerHTML).toBe(title);
+
+    const p = container.querySelector("p");
+    expect(p?.innerHTML).toBeUndefined;
+  });
+});
