81a96526fe4737f4cabbb3c59e601c4ccbd0c344
Axio Dev <axelvazmar0@gmail.com>
2026-05-31 00:19:35 -0600

    Iniating testing of the app

--- 03-gifs-app/vite.config.ts
+++ 03-gifs-app/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
-// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-// import babel from "@rolldown/plugin-babel";
+// import { defineConfig } from "vite";
+// // import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+// // import babel from "@rolldown/plugin-babel";
 
+// export default defineConfig({
+//   base: "./",
+// });
+
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
 });
