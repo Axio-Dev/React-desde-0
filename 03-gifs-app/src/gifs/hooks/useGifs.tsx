d086d87294f6657d656453bb73ae0659c26212f9
Axio Dev <axelvazmar0@gmail.com>
2026-05-30 23:26:39 -0600

    Using react hook useRef to caching

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { getGifsByQuery } from "../actions/get-gifs-by.query.action";
 import type { Gif } from "../interfaces/gif.interface";
 
 // Moving outside of the main hook to avoid re-rendering and avoid duplicate HTTP calls
-const gifsCache: Record<string, Gif[]> = {};
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
   const handleTermClicked = async (term: string) => {
-    if (gifsCache[term]) {
-      setGifs(gifsCache[term]);
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
       return;
     }
 
@@ -29,7 +31,7 @@ export const useGifs = () => {
     console.log(gifs);
     setGifs(gifs);
 
-    gifsCache[query] = gifs;
+    gifsCache.current[query] = gifs;
     console.log(gifsCache);
   };
 
