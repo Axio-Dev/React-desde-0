1e1523642226ffd5904d3aa6563292122cf3e699
Axio Dev <axelvazmar0@gmail.com>
2026-06-07 19:28:27 -0600

    Testing the debounce of the Search Bar

--- 03-gifs-app/src/shared/components/SearchBar.tsx
+++ 03-gifs-app/src/shared/components/SearchBar.tsx
@@ -15,8 +15,6 @@ export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
       onQuery(query);
     }, 700);
 
-    // onQuery(query);
-
     return () => {
       clearTimeout(timeOutId);
     };
