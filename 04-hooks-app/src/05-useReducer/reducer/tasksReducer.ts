358f902d9409374f68c38f28485d40c2fa55156c
Axio Dev <axelvazmar0@gmail.com>
2026-06-12 13:10:49 -0600

    Knowing reducer more depth

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -18,8 +18,56 @@ export type TaskAction =
 
 export const taskReduce = (state: TaskState, action: TaskAction): TaskState => {
   /**
-   * A reducer should always return a new state
+   * * A reducer should always return a new state
    */
 
-  return state;
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+
+      // ! DON'T DO THE FOLLOWING:
+      // state.todos.push(newTodo)
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.todos.length + 1,
+        pending: state.pending + 1,
+      };
+    }
+
+    case "DELETE_TODO":
+      const currentTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload,
+      );
+      return {
+        ...state,
+        todos: currentTodos,
+        length: currentTodos.length,
+        completed: currentTodos.filter((todo) => todo.completed).length,
+        pending: currentTodos.filter((todo) => !todo.completed).length,
+      };
+
+    case "TOGGLE_TODO":
+      const updatedTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: updatedTodos.filter((todo) => todo.completed).length,
+        pending: updatedTodos.filter((todo) => !todo.completed).length,
+      };
+
+    default:
+      return state;
+  }
+  1;
 };
