79ff38fa54a7817cf4099cfd9d5dd5e22bfd81ff
Axio Dev <axelvazmar0@gmail.com>
2026-06-20 14:34:04 -0600

    Implementando el lazyload para hacer que las páginas se carguen bajo demanda

--- 05-heroes-app/src/router/app.router.tsx
+++ 05-heroes-app/src/router/app.router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { lazy } from "react";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
