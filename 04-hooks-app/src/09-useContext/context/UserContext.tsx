4c7f831ddb418136078ad2087d588324f11725fd
Axio Dev <axelvazmar0@gmail.com>
2026-06-19 23:13:40 -0600

    Haciendo que el usuario permanezca con local storage y hacinedo la lógica del Logout

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, useState, type PropsWithChildren } from "react";
+import {
+  createContext,
+  useEffect,
+  useState,
+  type PropsWithChildren,
+} from "react";
 import { users, type User } from "../data/user-mock.data";
 
 type AuthStatus = "checking" | "authenticated" | "not-authenticated";
@@ -31,6 +36,7 @@ export const UserContextProvider = ({ children }: PropsWithChildren) => {
 
     setUser(user);
     setAuthStatus("authenticated");
+    localStorage.setItem("userId", userId.toString());
     return true;
   };
 
@@ -38,7 +44,15 @@ export const UserContextProvider = ({ children }: PropsWithChildren) => {
     console.log("Logout");
     setAuthStatus("not-authenticated");
     setUser(null);
+    localStorage.removeItem("userId");
   };
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId");
+    if (storedUserId) {
+      handleLogin(+storedUserId);
+    }
+  }, []);
 
   return (
     <UserContext
--- 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
+++ 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,9 +1,16 @@
 import { UserContext } from "@/09-useContext/context/UserContext";
 import { Button } from "@/components/ui/button";
 import { use } from "react";
+import { useNavigate } from "react-router";
 
 export const ProfilePage = () => {
-  const { user } = use(UserContext);
+  const { user, logout } = use(UserContext);
+  const navigation = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigation("/login");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
@@ -12,7 +19,9 @@ export const ProfilePage = () => {
 
       <pre className="my-4"> {JSON.stringify(user, null, 2)} </pre>
 
-      <Button variant="destructive">Salir</Button>
+      <Button variant="destructive" onClick={handleLogout}>
+        Salir
+      </Button>
     </div>
   );
 };
