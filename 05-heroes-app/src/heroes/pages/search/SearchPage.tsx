d06ab4394e8daddec4f056183df63d5ac9335d8c
Axio Dev <axelvazmar0@gmail.com>
2026-06-20 16:55:49 -0600

    Partiendo los componentes de búsqueda, filtros, filtros avanzados, etc

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
@@ -8,9 +9,10 @@ export const SearchPage = () => {
         title="Búsqueda de SuperHéroes"
         description="Descubre, explora y administrar SuperHéroes y Villanos"
       />
-
       {/* Stats Dashboard */}
       <HeroStats />
+      {/* Filters and Search */}
+      <SearchControls />
     </>
   );
 };
