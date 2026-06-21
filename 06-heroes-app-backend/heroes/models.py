e9287f7cc73c5cde4e5ebc0c98abc69894a05c35
Axio Dev <axelvazmar0@gmail.com>
2026-06-20 22:58:40 -0600

    First Commit

---
+++ 06-heroes-app-backend/heroes/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+import uuid
+
+class Universe(models.TextChoices):
+    MARVEL = 'MV', "Marvel"
+    DC = "DC" , "Detective Comics"
+    SU = "SU", "Sin Universo"
+
+# Create your models here.
+class Hero(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    alias = models.CharField(max_length=100)
+
+    powers = models.JSONField(default=list)
+
+    description = models.TextField()
+
+    strength = models.IntegerField()
+    intelligence = models.IntegerField()
+    speed = models.IntegerField()
+    durability = models.IntegerField()
+
+    team = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+
+    first_appearance = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    universe = models.CharField(
+        max_length=2,
+        choices=Universe.choices,
+        default=Universe.SU
+    )
