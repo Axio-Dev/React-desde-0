ded2d925416b13a5d439e03aade336ccd942097d
Axio Dev <axelvazmar0@gmail.com>
2026-06-22 22:37:25 -0600

    Apreniendo a hacer un details page y hacerlo dinámico

--- 05-heroes-app/src/heroes/components/HeroGridCard.tsx
+++ 05-heroes-app/src/heroes/components/HeroGridCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Brain, Eye, Gauge, Heart, Shield, Zap } from "lucide-react";
 import type { Hero } from "../types/hero.interface";
+import { useNavigate } from "react-router";
 
 interface Props {
   hero: Hero;
@@ -16,14 +17,21 @@ const universeNames: Record<string, string> = {
 };
 
 export const HeroGridCard = ({ hero }: Props) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/heroes/${hero.slug}`);
+  };
+
   const universeLabel = universeNames[hero.universe] ?? hero.universe;
   return (
     <Card className="group overflow-hidden hover:shadow-xl transition-all duration-300 hover:-translate-y-1 bg-gradient-to-br from-white to-gray-50">
-      <div className="relative h-64 overflow-hidden">
+      <div className="relative h-80">
         <img
           src={hero.image}
           alt={hero.name}
-          className="object-cover transition-all duration-500 group-hover:scale-110"
+          className="object-cover transition-all duration-500 group-hover:scale-110 absolute top[-30px] w-full h-[410px]"
+          onClick={handleClick}
         />
 
         {/* Status indicator */}
@@ -68,7 +76,7 @@ export const HeroGridCard = ({ hero }: Props) => {
         </Button>
       </div>
 
-      <CardHeader className="pb-3">
+      <CardHeader className="py-3 z-10 bg-gray-100/50 backdrop-blur-sm relative top-1 group-hover:top-[-10px] transition-all duration-300">
         <div className="flex justify-between items-start">
           <div className="space-y-1">
             <h3 className="font-bold text-lg leading-tight">{hero.alias}</h3>
@@ -83,7 +91,7 @@ export const HeroGridCard = ({ hero }: Props) => {
         </Badge>
       </CardHeader>
 
-      <CardContent className="space-y-4">
+      <CardContent className="space-y-2">
         <p className="text-sm text-gray-600 line-clamp-2">{hero.description}</p>
 
         {/* Stats */}
@@ -139,7 +147,7 @@ export const HeroGridCard = ({ hero }: Props) => {
           <h4 className="font-medium text-sm">Powers:</h4>
           <div className="flex flex-wrap gap-1">
             {hero.powers.slice(0, 3).map((power) => (
-              <Badge variant="outline" className="text-xs">
+              <Badge variant="outline" className="text-xs" key={power}>
                 {power}
               </Badge>
             ))}
