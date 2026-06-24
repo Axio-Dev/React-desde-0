806d886cedb8bdde4dcffc363d4084e5d3fa2043
Axio Dev <axelvazmar0@gmail.com>
2026-06-24 11:02:39 -0600

    Creando la paginación correct para que el front pueda leerlo como es debido

--- 06-heroes-app-backend/heroes/pagination.py
+++ 06-heroes-app-backend/heroes/pagination.py
@@ -1,4 +1,24 @@
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+import math
 
 class HeroPagination(LimitOffsetPagination):
     default_limit = 6
+    max_limit = 12
+
+    def get_paginated_response(self, data):
+        count = self.count
+        limit = self.limit or self.default_limit
+
+        total_pages = math.ceil(count / limit)
+
+        return Response({
+            "count": count,
+            "total_pages": total_pages,
+            "limit": limit,
+            "offset": self.offset,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data,
+        })
