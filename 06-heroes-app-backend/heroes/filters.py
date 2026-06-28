5a261588c53e534d461d2447c3a77e8e153999e0
Axio Dev <axelvazmar0@gmail.com>
2026-06-28 16:58:23 -0600

    feat(filters): añadiendo la posibilidad de que filters pueda buscar por nombre o alias al mismo tiempo

--- 06-heroes-app-backend/heroes/filters.py
+++ 06-heroes-app-backend/heroes/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from .models import Hero
 
@@ -16,7 +17,7 @@ class HeroFilter(django_filters.FilterSet):
         return queryset.filter(category__iexact=value)
 
 class HeroSearchFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr="icontains")
+    name = django_filters.CharFilter(method="filter_name")
     strength = django_filters.NumberFilter(field_name="strength", lookup_expr="gte")
     universe = django_filters.CharFilter(lookup_expr="iexact")
     
@@ -28,3 +29,8 @@ class HeroSearchFilter(django_filters.FilterSet):
             "universe",
             "status",
         ]
+    
+    def filter_name(self, queryset, name, value):
+        return queryset.filter(
+            Q(name__icontains=value) | Q(alias__icontains=value)
+        )
