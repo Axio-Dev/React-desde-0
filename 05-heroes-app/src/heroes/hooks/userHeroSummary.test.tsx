56177bd7fa05586aa575fd3ced8b0f1d54f8d92f
Axio Dev <axelvazmar0@gmail.com>
2026-07-02 14:47:22 -0600

    test(useHeroSummary): añade un test para el custom hook useHeroSummary

---
+++ 05-heroes-app/src/heroes/hooks/userHeroSummary.test.tsx
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+import { renderHook } from "@testing-library/react";
+import { useHeroSummary } from "./useHeroSummary";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { PropsWithChildren } from "react";
+
+const tanStackCustomProviders = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return ({ children }: PropsWithChildren) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+describe("useHeroSummary", () => {
+  test("should return the initial state (isLoading)", () => {
+    const { result } = renderHook(() => useHeroSummary(), {
+      wrapper: tanStackCustomProviders(),
+    });
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.isError).toBe(false);
+    // estos dos ultimos, son lo mismo, solo es otra manera
+    expect(result.current.data).toBe(undefined);
+    expect(result.current.data).toBeUndefined();
+  });
+});
